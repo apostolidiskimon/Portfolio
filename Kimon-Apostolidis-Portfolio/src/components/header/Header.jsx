b936304f9a6756293964db9e5c363f5e04a3ae52
KIMON APOSTOLIDIS <72627645+apostolidiskimon@users.noreply.github.com>
2023-01-10 22:27:30 +0200

    initial commit

---
+++ Kimon-Apostolidis-Portfolio/src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/placeholder1.jpg'
+import HeaderSocials from './HeaderSocials'
+const Header = () => {
+  return (
+<header>
+  <div className="container header_container">
+    <h5>Hello I'm</h5>
+    <h2>Kimon Apostolidis</h2>
+    <h5 className="textlight">
+      FullStack Developer
+    </h5>
+    <CTA/>
+     <HeaderSocials/>
+
+    <div className="me">
+      <img src={ME} alt="me" />
+    </div>
+
+    <a href="#contact" className='scroll_down'>Scroll Down</a>
+    
+  </div>
+</header>  )
+}
+
+export default Header
