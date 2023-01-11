bda2c9d9a9882676b491797d846b8273d4553993
KIMON APOSTOLIDIS <72627645+apostolidiskimon@users.noreply.github.com>
2023-01-11 21:27:36 +0200

    disbale eslint on run build

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} className="image-resize" alt="About Image" />
+            <img src={ME} className="image-resize" alt="About" />
           </div>
         </div>
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import './nav.css'
-import {AiFillHome} from 'react-icons/ai'
-import {FaUser} from 'react-icons/fa'
-import {ImBooks} from 'react-icons/im'
-import {FaTools} from 'react-icons/fa'
-import {IoMdContacts} from 'react-icons/io'
-import {useState} from 'react'
+import React from "react";
+import "./nav.css";
+import { AiFillHome } from "react-icons/ai";
+import { FaUser } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
+import { FaTools } from "react-icons/fa";
+import { IoMdContacts } from "react-icons/io";
+import { useState } from "react";
 const Nav = () => {
-  const[activeNav, setActiveNav]=useState('#')
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <nav> 
-      <a href="#" onClick={()=> setActiveNav('#')}className={activeNav==='#'? 'active':''       }><AiFillHome/></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')}className={activeNav==='#about'? 'active':''}><FaUser/></a>
-      <a href="#experience"onClick={()=> setActiveNav('#experience')}className={activeNav==='#experience'? 'active':''}><ImBooks/></a>
-      <a href="#services"onClick={()=> setActiveNav('#services')}className={activeNav==='#services'? 'active':''}><FaTools/></a>
-      <a href="#contact"onClick={()=> setActiveNav('#contact')}className={activeNav==='#contact'? 'active':''}><IoMdContacts/></a>
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiFillHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <ImBooks />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <FaTools />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <IoMdContacts />
+      </a>
+    </nav>
+  );
+};
 
-
-
-
- </nav>
-  )
-}
-
-export default Nav
+export default Nav;
