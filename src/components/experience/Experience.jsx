d2b7ac1360b5f14962838334dba10e249f7b2185
KIMON APOSTOLIDIS <72627645+apostolidiskimon@users.noreply.github.com>
2023-01-12 13:15:17 +0200

    Experience -Done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,44 +3,79 @@ import "./experience.css";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 const Experience = () => {
   return (
-    <section id="experience">
+    <section id="experience" className="responsive">
       <h5>The Skills I Have</h5>
       <h2>My experience</h2>
       <div className="container experience__container">
-        <div className="experience__frontend"></div>
-        <h3>Frontend Development</h3>
-        <div className="experience__content">
-          <article className="experience_details">
-            <BsFillCheckCircleFill />
-            <h4>HTML</h4>
-            <small ClassName="text-light">Competent</small>
-          </article>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>HTML</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
 
-          <article className="experience_details">
-            <BsFillCheckCircleFill />
-            <h4>CSS</h4>
-            <small ClassName="text-light">Competent</small>
-          </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>CSS</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
 
-          <article className="experience_details">
-            <BsFillCheckCircleFill />
-            <h4>JavaScript</h4>
-            <small ClassName="text-light">Competent</small>
-          </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>JavaScript</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
 
-          <article className="experience_details">
-            <BsFillCheckCircleFill />
-            <h4>React</h4>
-            <small ClassName="text-light">Competent</small>
-          </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>React</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
 
-          <article className="experience_details">
-            <BsFillCheckCircleFill />
-            <h4>Bootstrap</h4>
-            <small ClassName="text-light">Competent</small>
-          </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>Bootstrap</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
+          </div>
         </div>
-        <div className="Experience__backend"></div>
+        {/* FRONT END END */}
+        <div className="Experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>SQL </h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
+
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>Node Js </h4>
+              <small ClassName="text-light">Advanced Begginer</small>
+            </article>
+
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>Python</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
+
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>Django</h4>
+              <small ClassName="text-light">Competent</small>
+            </article>
+
+            <article className="experience_details">
+              <BsFillCheckCircleFill ClassName="experience__icons" />
+              <h4>Git</h4>
+              <small ClassName="text-light">Advanced Begginer</small>
+            </article>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { RiChatCheckLine } from "react-icons/ri";
 const Services = () => {
   return (
-    <section id='services'>Services</section>
-  )
-}
+    <section id="services">
+      <h5>What I Can Do</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service_head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX*/}
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <RiChatCheckLine className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END Web Development */}
+      </div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
